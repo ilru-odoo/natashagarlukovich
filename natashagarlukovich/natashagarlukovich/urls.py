a7d8429b468f37736aa55d9d09215050916d6741
LookHowFarWeveCome <henesy442@gmail.com>
2019-11-07 03:57:29 +0300

    Add app blog: init model Post, init html pages for blog and single

--- natashagarlukovich/natashagarlukovich/urls.py
+++ natashagarlukovich/natashagarlukovich/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
  
 urlpatterns = [
     path('',  include('portfolio.urls')),
+    path('blog/',  include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
  
