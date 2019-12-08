d2d68bd92b1a599d294c183857a57cf0fe08bb7b
LookHowFarWeveCome <henesy442@gmail.com>
2019-12-08 21:54:30 +0300

    add os secretekey

--- natashagarlukovich/natashagarlukovich/settings.py
+++ natashagarlukovich/natashagarlukovich/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #from natashagarlukovich.secret import *
 
-SECRET_KEY = secretkey
+SECRET_KEY = os.environ.get('secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
