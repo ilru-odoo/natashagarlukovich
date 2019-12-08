0988919701276d966bbacc8d35ad5b3e6589cec3
LookHowFarWeveCome <henesy442@gmail.com>
2019-12-08 21:08:34 +0300

    add heroku path to ALLOWED_HOSTS

--- natashagarlukovich/natashagarlukovich/settings.py
+++ natashagarlukovich/natashagarlukovich/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secretkey
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.1.30.33', '192.168.1.102']
+ALLOWED_HOSTS = ['127.0.0.1', '10.1.30.33', '192.168.1.102','natashagarlukovich.herokuapp.com']
 
 
 # Application definition
