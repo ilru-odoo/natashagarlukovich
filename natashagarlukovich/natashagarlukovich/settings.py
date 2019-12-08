befe6691c5bcb8627133a4e4b646206de67577ed
LookHowFarWeveCome <henesy442@gmail.com>
2019-12-08 23:16:04 +0300

    update Proc and settings

--- natashagarlukovich/natashagarlukovich/settings.py
+++ natashagarlukovich/natashagarlukovich/settings.py
@@ -141,6 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
 MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
